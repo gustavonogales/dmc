40ae3a7ab97097873d719f2987282759251966f5
Gustavo Nogales <gustavo.nogales@hotmail.com>
2023-09-30 21:42:53 -0300

    fix: siteUrl

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@
 module.exports = {
   siteMetadata: {
     title: 'Débora Martins Corrêa - Fisioterapeuta',
-    siteUrl: 'https://deboramartinscorrea.netlify.app/',
+    siteUrl: 'https://deborafisio.com',
     description:
       'Fisioterapeuta com atendimento domiciliar e humanizado. Especializada na reabilitação físico-motora e neurológica de pacientes idosos. Agende uma consulta de avaliação.',
   },
